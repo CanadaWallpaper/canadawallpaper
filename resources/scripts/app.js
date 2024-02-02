0516751a8d43f3d0e08d9893f6445dd278b34a7f
ivo-7 <m.bahiraie92@gmail.com>
2024-02-02 17:16:42 -0500

    Add brand slider

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,92 +1,197 @@
-const sliderWrapper = document.querySelector(".slider-wrapper");
-const slider = document.querySelector(".products");
-const previousButton = document.querySelector(".previous-button");
-const nextButton = document.querySelector(".next-button");
+//-START-// collection slider ////////////////////////////////////////////////////////////////////////////////
+const sliderWrapper = document.querySelector(
+  ".main-collection-container .slider-wrapper"
+);
+const slider = document.querySelector(".main-collection-container .products");
+const previousButton = document.querySelector(
+  ".main-collection-container .previous-button"
+);
+const nextButton = document.querySelector(
+  ".main-collection-container .next-button"
+);
 const sliderProducts = [...slider.children];
 var isPaused = false;
 
-const getProductsCardWidth = () => {
-  const productsCardWidth = document.querySelector(
-    ".product-card-wrapper"
-  ).offsetWidth;
-  return productsCardWidth;
-};
+const collectionSlider = () => {
+  // next slide function
 
-console.log(sliderProducts.length);
+  const getProductsCardWidth = () => {
+    const productsCardWidth = document.querySelector(
+      ".main-collection-container .product-card-wrapper"
+    ).offsetWidth;
+    return productsCardWidth;
+  };
 
-// next slide function
-const nextSlider = () => {
-  if (slider.scrollLeft === 0) {
-    slider.scrollLeft = getProductsCardWidth();
-  } else if (
-    Math.floor(slider.scrollLeft / getProductsCardWidth()) + 5 >=
-    sliderProducts.length
-  ) {
+  const nextSlider = () => {
+    if (slider.scrollLeft === 0) {
+      slider.scrollLeft = getProductsCardWidth();
+    } else if (
+      Math.floor(slider.scrollLeft / getProductsCardWidth()) + 5 >=
+      sliderProducts.length
+    ) {
+      slider.scrollLeft = 0;
+    } else {
+      slider.scrollLeft =
+        Math.floor(slider.scrollLeft / getProductsCardWidth()) *
+          getProductsCardWidth() +
+        getProductsCardWidth();
+    }
+  };
+
+  // previous slide function
+  const previousSlider = () => {
+    if (slider.scrollLeft === 0) {
+      slider.scrollLeft = sliderProducts.length * getProductsCardWidth();
+    } else {
+      slider.scrollLeft =
+        Math.ceil(slider.scrollLeft / getProductsCardWidth()) *
+          getProductsCardWidth() -
+        getProductsCardWidth();
+    }
+  };
+
+  nextButton.addEventListener("click", nextSlider);
+  previousButton.addEventListener("click", previousSlider);
+
+  window.addEventListener("resize", function () {
+    var pageWidth = window.innerWidth;
+
     slider.scrollLeft = 0;
-  } else {
-    slider.scrollLeft =
-      Math.floor(slider.scrollLeft / getProductsCardWidth()) *
-        getProductsCardWidth() +
-      getProductsCardWidth();
-  }
-
-  console.log();
-  console.log(sliderProducts.length);
-};
-
-// previous slide function
-const previousSlider = () => {
-  if (slider.scrollLeft === 0) {
-    slider.scrollLeft = sliderProducts.length * getProductsCardWidth();
-  } else {
-    slider.scrollLeft =
-      Math.ceil(slider.scrollLeft / getProductsCardWidth()) *
-        getProductsCardWidth() -
-      getProductsCardWidth();
-  }
-
-  console.log(slider.scrollLeft);
-};
-
-nextButton.addEventListener("click", nextSlider);
-previousButton.addEventListener("click", previousSlider);
-
-window.addEventListener("resize", function () {
-  var pageWidth = window.innerWidth;
-
-  slider.scrollLeft = 0;
-  // Your code to handle width change goes here
-  console.log(getProductsCardWidth());
-  console.log(pageWidth);
-});
-
-const productSlider = () => {
-  let intervalId;
-
-  const startInterval = () => {
-    intervalId = setInterval(() => {
-      nextSlider();
-    }, 2500);
-  };
-
-  const pauseInterval = () => {
-    clearInterval(intervalId);
-  };
-
-  startInterval(); // Start the interval initially
-
-  sliderWrapper.addEventListener("mouseenter", () => {
-    isPaused = true;
-    pauseInterval();
+    // Your code to handle width change goes here
+    console.log(getProductsCardWidth());
+    console.log(pageWidth);
   });
 
-  sliderWrapper.addEventListener("mouseleave", () => {
-    isPaused = false;
-    startInterval();
+  const productSlider = () => {
+    let intervalId;
+
+    const startInterval = () => {
+      intervalId = setInterval(() => {
+        nextSlider();
+      }, 2500);
+    };
+
+    const pauseInterval = () => {
+      clearInterval(intervalId);
+    };
+
+    startInterval(); // Start the interval initially
+
+    sliderWrapper.addEventListener("mouseenter", () => {
+      isPaused = true;
+      pauseInterval();
+    });
+
+    sliderWrapper.addEventListener("mouseleave", () => {
+      isPaused = false;
+      startInterval();
+    });
+  };
+
+  // window load events
+  window.addEventListener("load", (event) => {
+    productSlider();
   });
 };
+collectionSlider();
+//-END-// collection slider ////////////////////////////////////////////////////////////////////////////////
 
-// window load events
-window.addEventListener("load", (event) => {
-  productSlider();
-});
+//----------------------------------------------------------------------------------------------------------
+
+//-START-// brand slider ///////////////////////////////////////////////////////////////////////////////////
+const brandsSliderWrapper = document.querySelector(
+  ".main-brands-container .brand-slider-wrapper"
+);
+const brandsSlider = document.querySelector(".main-brands-container .brands");
+const brandsPreviousButton = document.querySelector(
+  ".main-brands-container .previous-button-brand"
+);
+const brandsNextButton = document.querySelector(
+  ".main-brands-container .next-button-brand"
+);
+const sliderBrands = [...brandsSlider.children];
+var isPausedBrand = false;
+
+const brandSlider = () => {
+  const getBrndsCardWidth = () => {
+    const brandsCardWidth = document.querySelector(
+      ".main-brands-container  .brand-card-wrapper"
+    ).offsetWidth;
+    return brandsCardWidth;
+  };
+
+  // next slide function
+  const nextSlider = () => {
+    if (brandsSlider.scrollLeft === 0) {
+      brandsSlider.scrollLeft = getBrndsCardWidth();
+    } else if (
+      Math.floor(brandsSlider.scrollLeft / getBrndsCardWidth()) + 5 >=
+      sliderBrands.length
+    ) {
+      brandsSlider.scrollLeft = 0;
+    } else {
+      brandsSlider.scrollLeft =
+        Math.floor(brandsSlider.scrollLeft / getBrndsCardWidth()) *
+          getBrndsCardWidth() +
+        getBrndsCardWidth();
+    }
+  };
+
+  // previous slide function
+  const previousSlider = () => {
+    if (brandsSlider.scrollLeft === 0) {
+      brandsSlider.scrollLeft = sliderBrands.length * getBrndsCardWidth();
+    } else {
+      brandsSlider.scrollLeft =
+        Math.ceil(brandsSlider.scrollLeft / getBrndsCardWidth()) *
+          getBrndsCardWidth() -
+        getBrndsCardWidth();
+    }
+  };
+
+  brandsNextButton.addEventListener("click", nextSlider);
+  brandsPreviousButton.addEventListener("click", previousSlider);
+
+  window.addEventListener("resize", function () {
+    var pageWidth = window.innerWidth;
+
+    brandsSlider.scrollLeft = 0;
+    // Your code to handle width change goes here
+    console.log(getBrndsCardWidth());
+    console.log(pageWidth);
+  });
+
+  // const brandSlider = () => {
+  //   let intervalId;
+
+  //   const startInterval = () => {
+  //     intervalId = setInterval(() => {
+  //       nextSlider();
+  //     }, 2500);
+  //   };
+
+  //   const pauseInterval = () => {
+  //     clearInterval(intervalId);
+  //   };
+
+  //   startInterval(); // Start the interval initially
+
+  //   brandsSliderWrapper.addEventListener("mouseenter", () => {
+  //     isPausedBrand = true;
+  //     pauseInterval();
+  //   });
+
+  //   brandsSliderWrapper.addEventListener("mouseleave", () => {
+  //     isPausedBrand = false;
+  //     startInterval();
+  //   });
+  // };
+
+  // // window load events
+  // window.addEventListener("load", (event) => {
+  //   brandSlider();
+  // });
+
+  //-END-// brand slider ////////////////////////////////////////////////////////////////////////////////
+};
+brandSlider();
